1fdc5248135e93d35a59ebf7b4720d792b62488d
Paweł Wilczek <5646246+beowoolf@users.noreply.github.com>
2026-03-15 20:37:43 +0100

    Added example Nuxt.js project to test Content module

---
+++ components/ShowPostTags.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mt-2">
+    <span v-for="tag in tags" :key="tag" class="text-muted">
+      <InternalLink
+        class="btn btn-link font-weight-bold"
+        :class="
+          $colorMode.preference == 'light' ? 'text-primary' : 'text-warning'
+        "
+        :page="`/blog/category/${tag}`"
+        :anchor="tag"
+      >
+        {{ tag }}
+      </InternalLink>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tags: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
---
+++ components/ShowPosts.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1 class="text-center"><slot>Wpisy</slot></h1>
+    <BootstrapRow>
+      <div
+        v-for="post in posts"
+        :key="post.slug"
+        class="transition-all duration-500 ease-in-out col-sm-4 col-12"
+      >
+        <InternalLink
+          class="text-muted"
+          :page="`/blog/${post.slug}`"
+          :anchor="post.title"
+        >
+          <nuxt-img
+            class="img-fluid"
+            :src="post.img"
+            :alt="post.alt"
+            :title="post.alt"
+            preset="preview"
+            format="webp"
+            width="800"
+            height="600"
+            preload
+          />
+        </InternalLink>
+        <ShowPostTags :tags="post.tags" />
+        <InternalLink
+          :page="`/blog/${post.slug}`"
+          :anchor="post.title"
+          :class="$colorMode.preference == 'light' ? 'text-dark' : 'text-white'"
+        >
+          <h2 class="mt-2">
+            {{ post.title }}
+          </h2>
+        </InternalLink>
+      </div>
+    </BootstrapRow>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
